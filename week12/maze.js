ff8de07ad932af3651573f40c94a262eb8c17e65
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-04-20 10:08:10 -0400

    Update maze.js

--- week12/maze.js
+++ week12/maze.js
@@ -4,9 +4,12 @@ var stat = $("#status");
 var stat2 = $("#status2");
 var boundary = $(".boundary");
 var body = $(".body");
+var body3 = $(".body3");
 var gameStart = false;
-var start =  $("#start").click(function(){
+var start = $("#start").click(function(){
     gameStart = true;
+    body.css("background-color", "lightblue");
+
     boundary.css("background-color", "black")
     if(boundary.hasClass('lost')){
         boundary.removeClass('lost');
@@ -22,6 +25,9 @@ function lost(){
         gameStart = false;
         console.log("hi");
         body.css("background-color", "red");
+        body3.css("background-color", "red");
+        $('#lose')[0].play()
+
         stat.text('YOU LOST!');
         stat2.text('tap yellow bar to restart')
     }
@@ -31,7 +37,17 @@ function won(){
     if(gameStart == true){
         gameStart = false;
         stat.text('YOU WON!');
-        body.css("background-color", "green");
+        body.css("background-color", "green");       body3.css("background-image", "url('https://i.pinimg.com/originals/49/07/82/49078239072dee326254d6a0d1a34ea9.gif')");
+        $('#work')[0].play()
+        if($('body').is('.body3')){
+            stat.text('YOU FINISHED THE GAME!');
+
+        }
+
+        
+
         stat2.text('tap yellow bar to restart')
+
+
     }
 }
